6722eeda97e3ab2a0c45d42455ee61a539053d29
minhng92 <minhng92@gmail.com>
2019-01-16 10:18:04 +0700

    remove sudo

--- install.sh
+++ install.sh
@@ -2,14 +2,14 @@
 
 # Note: run with root permission
 # Created symlink from /etc/systemd/system/multi-user.target.wants/ivas.service to /etc/systemd/system/ivas.service.
-sudo chmod +x src/main.py
-sudo chmod 777 src/default.jpg
-sudo chmod 777 src/config.yaml
-sudo chmod -R 777 src/images
+chmod +x src/main.py
+chmod 777 src/default.jpg
+chmod 777 src/config.yaml
+chmod -R 777 src/images
 
 python generate_service.py
-sudo cp mjpeg_optimized.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable mjpeg_optimized.service
-sudo systemctl start mjpeg_optimized
-sudo systemctl status mjpeg_optimized
+cp mjpeg_optimized.service /etc/systemd/system
+systemctl daemon-reload
+systemctl enable mjpeg_optimized.service
+systemctl start mjpeg_optimized
+systemctl status mjpeg_optimized
